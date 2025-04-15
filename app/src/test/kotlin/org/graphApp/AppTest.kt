c670c278b7586a654b15a02c371900a80f0d543a
vlapugb <vladimirpugovkinspb@gmail.com>
2025-04-15 19:11:38 +0300

    fix: delete test example

--- app/src/test/kotlin/org/graphApp/AppTest.kt
+++ app/src/test/kotlin/org/graphApp/AppTest.kt
@@ -7,9 +7,4 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertNotNull
 
 class AppTest {
-    @Test
-    fun appHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
-    }
 }
