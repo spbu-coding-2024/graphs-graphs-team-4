2528f088b99e9d7ff242d42dfaf686d3d8df977a
ganesha <tatiana.gromova2022@yandex.ru>
2025-04-18 19:24:31 +0300

    feat: implement theme support

--- app/src/main/kotlin/org/graphApp/view/theme/Theme.kt
+++ app/src/main/kotlin/org/graphApp/view/theme/Theme.kt
@@ -1,2 +1,48 @@
 package org.graphApp.view.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+// нужно еще будет реализовать переключение темы, а еще цвета подобрать
+val LightColorScheme = lightColors(
+    primary = PrimaryColor,
+    primaryVariant = ButtonColor,
+    background = FrameColor,
+    surface = DarkGray,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+val DarkColorScheme = darkColors(
+    primary = PrimaryColor,
+    background = FrameColor,
+    surface = DarkGray,
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+)
+
+@Composable
+fun GraphTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colors = colorScheme,
+        typography = replyTypography,
+        shapes = replyShapes,
+        content = content
+    )
+}
+
+
+
