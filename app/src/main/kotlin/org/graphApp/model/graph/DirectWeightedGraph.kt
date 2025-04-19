23f687c329cb790906c3d3d8304b68d238fec933
silvitreatment <1junyawork@gmail.com>
2025-04-19 16:53:53 +0300

    fix : Id Long type

--- app/src/main/kotlin/org/graphApp/model/graph/DirectWeightedGraph.kt
+++ app/src/main/kotlin/org/graphApp/model/graph/DirectWeightedGraph.kt
@@ -1,22 +1,28 @@
 package org.graphApp.model.graph
 
 internal class DirectWeightedGraph<V, E> : DirectedWeightedGraph<V, E> {
-    private val _vertices = hashMapOf<V, Vertex<V>>()
+    private val _vertices = hashMapOf<Long, Vertex<V>>()
+    private val elementToVertex = hashMapOf<V, Vertex<V>>()
     private val _edges = hashMapOf<E, WeightedDirectedEdge<E, V>>()
+    private var vertexIdCounter: Long = 0
 
     override val vertices: Collection<Vertex<V>> get() = _vertices.values
     override val edges: Collection<Edge<E, V>> get() = _edges.values
 
-    override fun addVertex(v: V, id: String) =
-        _vertices.getOrPut(v) { DVertex(id, v) }
+    override fun addVertex(v: V): Vertex<V> =
+        elementToVertex.getOrPut(v) {
+            val newVertex = DVertex(vertexIdCounter++, v)
+            _vertices[newVertex.id] = newVertex
+            newVertex
+        }
 
-    override fun addEdge(u: Pair<String, V>, v: Pair<String, V>, e: E): Edge<E, V> =
+    override fun addEdge(u: V, v: V, e: E): Edge<E, V> =
         throw IllegalArgumentException("You must set weight for your edge")
 
-    override fun addEdge(u: Pair<String, V>, v: Pair<String, V>, e: E, weight: String): WeightedDirectedEdge<E, V> {
-        val first = addVertex(u.second, u.first)
-        val second = addVertex(v.second, v.first)
-        return _edges.getOrPut(e) { DataWeightedDirectedEdge(e, first, second, weight) }
+    override fun addEdge(u: V, v: V, e: E, weight: String): WeightedDirectedEdge<E, V> {
+        val from = addVertex(u)
+        val to = addVertex(v)
+        return _edges.getOrPut(e) { DataWeightedDirectedEdge(e, from, to, weight) }
     }
 
     private data class DataWeightedDirectedEdge<E, V>(
