67f9cdf12e9d59f1ce6684c0980b21c37ad360df
vlapugb <vladimirpugovkinspb@gmail.com>
2025-05-13 23:44:21 +0300

    add: bfsspanning tree algo

--- app/src/main/kotlin/org/graphApp/model/graph/algorithms/minimalSpanningTree.kt
+++ app/src/main/kotlin/org/graphApp/model/graph/algorithms/minimalSpanningTree.kt
@@ -3,6 +3,10 @@ package org.graphApp.model.graph.algorithms
 import org.graphApp.model.graph.Graph
 import org.graphApp.model.graph.Vertex
 import org.graphApp.model.graph.WeightedEdge
+import org.jetbrains.exposed.sql.Query
+
+
+
 
 class MinimalSpanningTree<V, E>(
     private val graph: Graph<V,E>,
@@ -11,12 +15,18 @@ class MinimalSpanningTree<V, E>(
 ) {
     private val _edges = graph.edges
     private val _vertices = graph.vertices
+    private var _used: MutableMap<Long, Boolean> = mutableMapOf()
+
     private val _sortedEdges: List<WeightedEdge<E, V>> =
         _edges
             .filterIsInstance<WeightedEdge<E, V>>()
             .sortedBy { it.weight.toDouble() }
 
-
+    private fun resetUsedFlags() {
+        _used = graph.vertices.associate {
+                vertex -> vertex.id to false
+        }.toMutableMap()
+    }
     private val parent = mutableMapOf<Long, Long>()
 
     private fun dsuInit() {
@@ -42,17 +52,31 @@ class MinimalSpanningTree<V, E>(
 
 
     private fun bfsSpanningTree (
-
     ) : List<WeightedEdge<E,V>>? {
-        return null
+        resetUsedFlags()
+        val result = mutableListOf<WeightedEdge<E,V>>()
+        val queue: ArrayDeque<WeightedEdge<E,V>> = ArrayDeque()
+        queue.add(_sortedEdges[0])
+        _used[_sortedEdges[0].vertices.first.id] = true
+        while(queue.isNotEmpty()) {
+            val currentEdge = queue.first()
+            queue.removeFirst()
+            _sortedEdges
+                .filter { it.vertices.first.id == currentEdge.vertices.first.id }
+                .forEach { edge ->
+                    result.add(edge)
+                    if(_used[currentEdge.vertices.second.id] != false) {
+                        queue.add(edge)
+                        _used[edge.vertices.first.id] = true
+                    }
+                }
+        }
+        return result
     }
-
-
-
 
     private fun kraskalSpanningTree () : List<WeightedEdge<E,V>>? {
         dsuInit()
-        var cost: Int = 0
+        var cost = 0
         val res = mutableListOf<WeightedEdge<E,V>>()
 
         _sortedEdges.forEach { edge ->
@@ -80,7 +104,6 @@ fun minimalSpanningTree (
     } else {
         return bfsSpanningTree()
     }
-
 
     }
 }
