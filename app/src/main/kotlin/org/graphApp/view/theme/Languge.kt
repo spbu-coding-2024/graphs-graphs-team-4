5b16167da0b4eb69f7abae4e5e821fbbd6d9ce83
silvitreatment <1junyawork@gmail.com>
2025-05-21 00:53:18 +0300

    add: Chinese language

--- app/src/main/kotlin/org/graphApp/view/theme/Languge.kt
+++ app/src/main/kotlin/org/graphApp/view/theme/Languge.kt
@@ -1,13 +1,11 @@
 package org.graphApp.model
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
-import org.graphApp.view.components.addFolder
-import javax.management.monitor.StringMonitor
 
 enum class AppLanguage {
     ENGLISH,
-    RUSSIAN
+    RUSSIAN,
+    CHINESE
 }
 data class TextResources(
     val fileMenu: String,
@@ -38,6 +36,7 @@ data class TextResources(
 
     val english: String,
     val russian: String,
+    val chinese : String,
 
     val themeLight: String,
     val themeDark: String,
@@ -69,7 +68,22 @@ data class TextResources(
     val addFolder : String,
     val showWeights : String,
     val showVertexLabels : String,
-    val load : String
+    val load : String,
+
+    val guide : String,
+    val createVertex : String,
+    val rightClick1 : String,
+    val createEdge : String,
+    val rightClick2 : String,
+    val editElements: String,
+    val doubleClick : String,
+    val deleteElements : String,
+    val clickOnVertex : String,
+    val additional : String,
+    val newGraphFile: String,
+
+
+
 )
 
 val englishResources = TextResources(
@@ -101,6 +115,7 @@ val englishResources = TextResources(
 
     english = "English",
     russian = "Russian",
+    chinese = "Chinese",
 
     themeLight = "Light",
     themeDark = "Dark",
@@ -135,7 +150,35 @@ val englishResources = TextResources(
     showWeights = "Edge weights",
     showVertexLabels = "Vertex labels",
 
-    load = "Load"
+    load = "Load",
+
+    guide = "Graph Creation Guide",
+    createVertex = "Create Vertex:",
+    rightClick1 = """  
+                                1) Right-click on empty area
+                                2) Enter vertex value in the pop-up window
+                        """,
+    createEdge = "Create Edge:",
+    rightClick2 = """
+                                1) Right-click first vertex (source)
+                                2) Right-click second vertex (target):
+                                        - Directed graph: edge from 1st → 2nd vertex
+                                        - Undirected graph: bidirectional edge 
+                        """,
+    editElements = "Edit Elements:",
+    doubleClick = """
+                                1) Double-click vertex ID or edge weight
+                                2) Enter new value
+                                """,
+    deleteElements = "Delete Elements:",
+    clickOnVertex = """
+                                1) Click on vertex/edge to select
+                                2) Press Delete key
+                            """,
+    additional = "Additional:",
+    newGraphFile = """
+                                1) New Graph: File → New Graph
+                            """
 )
 
 val russianResources = TextResources(
@@ -167,6 +210,7 @@ val russianResources = TextResources(
 
     english = "Английский",
     russian = "Русский",
+    chinese = "Китайский",
 
     themeLight = "Светлая",
     themeDark = "Тёмная",
@@ -201,14 +245,137 @@ val russianResources = TextResources(
     showWeights = "Веса ребер",
     showVertexLabels = "Лейблы вершин",
 
-    load = "Открыть"
+    load = "Открыть",
 
+    guide = "Руководство по созданию графиков",
+    createVertex = "Создать вершину:",
+    rightClick1 = """  
+                                1) Щелкните правой кнопкой мыши по пустой области
+                                2) Введите значение вершины во всплывающем окне
+                        """,
+    createEdge = "Создать ребро:",
+    rightClick2 = """
+                                1) Щелкните правой кнопкой мыши первую вершину (источник)
+                                2) Щелкните правой кнопкой мыши вторую вершину (цель):
+                                    - Направленный граф: ребро от 1-й → 2-й вершины
+                                    - Ненаправленный граф: двунаправленное ребро
+                        """,
+    editElements = "Редактировать элементы: ",
+    doubleClick = """
+                                1) Дважды щелкните идентификатор вершины или вес ребра
+                                2) Введите новое значение
+                                """,
+    deleteElements = "Удалить элементы:",
+    clickOnVertex = """
+                                1) Щелкните вершину/ребро, чтобы выбрать
+                                2) Нажмите клавишу Delete
+                            """,
+    additional = "Дополнительный:",
+    newGraphFile = """
+                                1) Новый график: Файл → Новый график
+                            """
+
+)
+val chineseResources = TextResources(
+    fileMenu = "文件",
+    editMenu = "编辑",
+    viewMenu = "视图",
+    algorithmsMenu = "算法",
+    settingsMenu = "设置",
+    helpMenu = "帮助",
+
+    newGraph = "新建图表",
+    open = "打开",
+    recentGraphs = "最近打开的图表:",
+    loadGraph = "加载图表",
+    save = "保存",
+    saveAs = "另存为",
+    reset = "重置",
+
+    undo = "撤销",
+    redo = "重做",
+
+    settings = "设置",
+    resetDefault = "恢复默认设置",
+
+    language = "语言",
+
+    aboutProgram = "关于程序",
+    quickStart = "快速入门",
+
+    english = "英文",
+    russian = "俄文",
+    chinese = "中文",
+
+    themeLight = "浅色",
+    themeDark = "深色",
+
+    algorithmsDialog = "算法",
+    basic = "基础",
+    classic = "经典",
+    stronglyConnected = "强连通分量",
+    findCycles = "查找循环",
+    minimalTree = "最小生成树",
+    fordBellman = "福特-贝尔曼算法",
+    layout = "平面图布局",
+    findCommunities = "查找社区",
+    startAlgo = "开始",
+
+    viewDialog = "视图",
+    theme = "主题:",
+    cancel = "取消",
+    ok = "确定",
+    light = "浅色",
+    dark = "深色",
+
+    close = "关闭",
+    chooseType = "选择类型",
+    weights = "权重",
+    directed = "有向图",
+    createGraph = "创建图表",
+
+    enterGraphName = "输入图表名称",
+    chooseFormat = "选择保存格式:",
+    addFolder = "添加文件夹",
+    showWeights = "显示边权重",
+    showVertexLabels = "显示顶点标签",
+
+    load = "加载",
+
+    guide = "图表创建指南",
+    createVertex = "创建顶点:",
+    rightClick1 = """  
+                                1) 在空白处右键点击
+                                2) 在弹出窗口中输入顶点值
+                        """,
+    createEdge = "创建边:",
+    rightClick2 = """
+                                1) 右键点击第一个顶点(源)
+                                2) 右键点击第二个顶点(目标):
+                                        - 有向图: 边从第1个顶点指向第2个顶点
+                                        - 无向图: 双向边 
+                        """,
+    editElements = "编辑元素:",
+    doubleClick = """
+                                1) 双击顶点ID或边权重
+                                2) 输入新值
+                                """,
+    deleteElements = "删除元素:",
+    clickOnVertex = """
+                                1) 点击顶点/边进行选择
+                                2) 按下删除键
+                            """,
+    additional = "附加说明:",
+    newGraphFile = """
+                                1) 新建图表: 文件 → 新建图表
+                            """
 )
 
 fun getResources(language: AppLanguage): TextResources {
     return when (language) {
         AppLanguage.ENGLISH -> englishResources
         AppLanguage.RUSSIAN -> russianResources
+        AppLanguage.CHINESE -> chineseResources
     }
 }
 
