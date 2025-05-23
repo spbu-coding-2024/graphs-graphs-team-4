9fa79bc84b2090370ce7eaa8ea936683e0f02c6c
vlapugb <vladimirpugovkinspb@gmail.com>
2025-05-24 02:13:42 +0300

    add: implementation for uploading and storing graph

--- app/src/main/kotlin/org/graphApp/data/Neo4j/Neo4j.kt
+++ app/src/main/kotlin/org/graphApp/data/Neo4j/Neo4j.kt
@@ -1,87 +1,238 @@
 package org.graphApp.data.Neo4j
 
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.graphApp.model.graph.DirectedEdge
 import org.graphApp.model.graph.Edge
 import org.graphApp.model.graph.Vertex
 import org.graphApp.model.graph.WeightedDirectedEdge
 import org.graphApp.model.graph.WeightedEdge
+import org.graphApp.viewmodel.MainScreenViewModel
+import org.graphApp.viewmodel.graph.EdgeViewModel
 import org.graphApp.viewmodel.graph.GraphViewModel
+import org.graphApp.viewmodel.graph.VertexViewModel
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.GraphDatabase
 
-class Neo4jDataBase<V,E> (
-    val viewModel : GraphViewModel<V,E>,
+const val STUB = 0
+
+
+class Neo4jDataBase<V, E>(
+    val graphViewModel: GraphViewModel<V, E>,
+    val mainViewModel: MainScreenViewModel<E>,
     uri: String,
     username: String,
     password: String,
 ) {
-    private val driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
+
+
+    private val driver = GraphDatabase.driver(uri.ifBlank { "bolt://localhost:7687" }, AuthTokens.basic(username, password))
     private val session = driver.session()
 
-    fun addVertex (vertex: Vertex<V>) {
-        val params = mapOf(
-            "id" to vertex.id,
-            "element" to vertex.element
-        )
-        session.executeWrite { tx ->
-            tx.run("CREATE (node: Vertex {id: \$id, element: \$element})" +
-                    "RETURN node", params)
 
+    enum class EdgeTypeNeo4j(val typeString: String) {
+        DIRECTED("directedunweighted"),
+        DIRECTED_WEIGHTED("directedweighted"),
+        UNDIRECTED("undirectedunweighted"),
+        WEIGHTED("undirectedweighted")
+    }
+
+
+    private fun storeVertex(vertex: VertexViewModel<V>, onComplete: () -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val params = mapOf(
+                "id" to vertex.vertexID,
+                "element" to vertex.vertex.element
+            )
+            session.executeWrite { tx ->
+                tx.run(
+                    "CREATE (node: Vertex {id: \$id, element: \$element})", params
+                )
+
+            }
         }
     }
 
-    fun addEdge (edge: Edge<E, V>) {
-        val edgeType = when(edge) {
-            is WeightedDirectedEdge<E, V> -> {
+    private fun storeEdge(edge: EdgeViewModel<E, V>, onComplete: () -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (graphViewModel.isWeightedGraph.value && graphViewModel.isDirectedGraph.value) {
                 val paramsWeightedDirectedEdge = mapOf(
-                    "fromID" to edge.from.id,
-                    "toID" to edge.to.id,
-                    "element" to edge.element,
-                    "weight" to edge.weight
+                    "fromID" to edge.u.vertexID,
+                    "toID" to edge.v.vertexID,
+                    "weight" to edge.weight,
+                    "type" to EdgeTypeNeo4j.DIRECTED_WEIGHTED.typeString
                 )
-                session.executeWrite { tx ->
-                    tx.run("MATCH(from: Vertex {id: \$fromID}), (to: Vertex {id: \$toID})" +
-                            "CREATE (from)-[:CONNECTED_TO {element: \$element, weight: \$weight}]->(to)", paramsWeightedDirectedEdge)
+                driver.session().executeWrite { tx ->
+                    tx.run(
+                        "MATCH (from: Vertex {id: \$fromID}), (to: Vertex {id: \$toID})" +
+                                "CREATE (from)-[:CONNECTED_TO {weight: \$weight, type: \$type}]->(to)",
+                        paramsWeightedDirectedEdge
+                    )
                 }
 
             }
-            is DirectedEdge<E,V> -> {
+            if (!graphViewModel.isWeightedGraph.value && graphViewModel.isDirectedGraph.value) {
                 val directedEdge = mapOf(
-                    "fromID" to edge.from.id,
-                    "toID" to edge.to.id,
-                    "element" to edge.element,
+                    "fromID" to edge.u.vertexID,
+                    "toID" to edge.v.vertexID,
+                    "type" to EdgeTypeNeo4j.DIRECTED.typeString
                 )
-                session.executeWrite { tx ->
-                    tx.run("MATCH(from: Vertex {id: \$fromID}), (to: Vertex {id: \$toID})" +
-                            "CREATE (from)-[:CONNECTED_TO {element: \$element}]->(to)", directedEdge)
+                driver.session().executeWrite { tx ->
+                    tx.run(
+                        "MATCH (from: Vertex {id: \$fromID}), (to: Vertex {id: \$toID})" +
+                                "CREATE (from)-[:CONNECTED_TO {type: \$type}]->(to)", directedEdge
+                    )
                 }
 
             }
-            is WeightedEdge<E, V> -> {
+            if (graphViewModel.isWeightedGraph.value && !graphViewModel.isDirectedGraph.value) {
                 val paramsWeightedEdge = mapOf(
-                    "V1" to edge.vertices.first,
-                    "V2" to edge.vertices.second,
-                    "element" to edge.element,
-                    "weight" to edge.weight
+                    "V1" to edge.u.vertexID,
+                    "V2" to edge.v.vertexID,
+                    "weight" to edge.weight,
+                    "type" to EdgeTypeNeo4j.WEIGHTED.typeString
                 )
-                session.executeWrite { tx ->
-                    tx.run("MATCH(V1: Vertex {id: \$Vertex1ID}), (V2: Vertex {id: \$Vertex2ID})" +
-                    "CREATE (V1)-[:CONNECTED_TO {element: \$element, weight: \$weight}]-(V2)", paramsWeightedEdge)
+                driver.session().executeWrite { tx ->
+                    tx.run(
+                        "MATCH (Vertex1: Vertex {id: \$V1}), (Vertex2: Vertex {id: \$V2})" +
+                                "CREATE (Vertex1)-[:CONNECTED_TO {weight: \$weight, type: \$type}]-(Vertex2)",
+                        paramsWeightedEdge
+                    )
                 }
             }
 
-            else -> {
+            if (!graphViewModel.isWeightedGraph.value && !graphViewModel.isDirectedGraph.value) {
                 val paramsEdge = mapOf(
-                    "Vertex1ID" to edge.vertices.first.id,
-                    "Vertex2ID" to edge.vertices.second.id,
-                    "element" to edge.element
+                    "Vertex1ID" to edge.u.vertexID,
+                    "Vertex2ID" to edge.v.vertexID,
+                    "type" to EdgeTypeNeo4j.UNDIRECTED.typeString
                 )
-                session.executeWrite { tx ->
-                    tx.run("MATCH(V1: Vertex {id: \$Vertex1ID}), (V2: Vertex {id: \$Vertex2ID})" +
-                            "CREATE (V1)-[:CONNECTED_TO {element: \$element}]-(V2)", paramsEdge)
+                driver.session().executeWrite { tx ->
+                    tx.run(
+                        "MATCH(V1: Vertex {id: \$Vertex1ID}), (V2: Vertex {id: \$Vertex2ID})" +
+                                "CREATE (V1)-[:CONNECTED_TO {type: \$type}]-(V2)", paramsEdge
+                    )
                 }
             }
         }
+    }
+
+    fun storeGraph() {
+        val vertices = graphViewModel.vertices
+        val edges = graphViewModel.edges
+        vertices.forEach { vertex ->
+            storeVertex(vertex, onComplete = {
+                edges.forEach { edge ->
+                    storeEdge(edge, {})
+                }
+            })
+        }
+
 
     }
+
+
+    private fun uploadVertex(onComplete: () -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            driver.session().executeRead { tx ->
+                val unloadVertices = tx.run(
+                    "MATCH (node:Vertex) RETURN node.id as id, node.element as element"
+                )
+                unloadVertices.forEach { vertex ->
+                    val element = vertex["element"].asObject()
+                    graphViewModel.addVertex(element as V, 0.dp, 0.dp)
+                }
+            }
+        }
+    }
+
+    private fun uploadEdge(onComplete: () -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            driver.session().executeRead { tx ->
+                val unloadEdges = tx.run(
+                    "MATCH (a)-[edge:CONNECTED_TO]-(b)" + "RETURN edge.type AS type"
+                )
+                val mapForCorrectTypeEdge: MutableMap<String, Int> = mutableMapOf()
+                unloadEdges.forEach { edge ->
+                    val typeOfEdge = edge["type"].asString()
+                    mapForCorrectTypeEdge.put(typeOfEdge, STUB)
+                }
+                if (mapForCorrectTypeEdge.size != 1) {
+                    throw IllegalArgumentException("Incorrect type of edge")
+                } else {
+                    when (mapForCorrectTypeEdge.keys.first()) {
+
+                        EdgeTypeNeo4j.DIRECTED_WEIGHTED.typeString -> {
+                            mainViewModel.isWeightedGraph = true
+                            mainViewModel.isDirectedGraph = true
+                            val unloadAllEdges = tx.run(
+                                "MATCH (a:Vertex)-[edge:CONNECTED_TO]->(b:Vertex)" + "RETURN edge.type AS type, edge.weight AS weight, a.id AS idFrom, b.id AS idTo)",
+                            )
+                            unloadAllEdges.forEach { edge ->
+                                val IDFrom = edge["idFrom"].toString().toLong()
+                                val IDTo = edge["idTo"].toString().toLong()
+                                val edgeWeight = edge["weight"].asString()
+                                graphViewModel.createEdge(IDFrom, IDTo, edgeWeight)
+                            }
+                        }
+
+                        EdgeTypeNeo4j.WEIGHTED.typeString -> {
+                            mainViewModel.isWeightedGraph = true
+                            mainViewModel.isDirectedGraph = false
+                            val unloadAllEdges = tx.run(
+                                "MATCH (a:Vertex)-[edge:CONNECTED_TO]-(b:Vertex)" + "RETURN edge.type AS type, edge.weight AS weight, a.id AS idFrom, b.id AS idTo)",
+                            )
+                            unloadAllEdges.forEach { edge ->
+                                val IDFrom = edge["idFrom"].toString().toLong()
+                                val IDTo = edge["idTo"].toString().toLong()
+                                val edgeWeight = edge["weight"].asString()
+                                graphViewModel.createEdge(IDFrom, IDTo, edgeWeight)
+                            }
+                        }
+
+                        EdgeTypeNeo4j.DIRECTED.typeString -> {
+                            mainViewModel.isWeightedGraph = false
+                            mainViewModel.isDirectedGraph = true
+                            val unloadAllEdges = tx.run(
+                                "MATCH (a:Vertex)-[edge:CONNECTED_TO]->(b:Vertex)" + "RETURN edge.type AS type, a.id AS idFrom, b.id AS idTo)",
+                            )
+                            unloadAllEdges.forEach { edge ->
+                                val IDFrom = edge["idFrom"].toString().toLong()
+                                val IDTo = edge["idTo"].toString().toLong()
+                                graphViewModel.createEdge(IDFrom, IDTo, null)
+                            }
+                        }
+
+                        EdgeTypeNeo4j.UNDIRECTED.typeString -> {
+                            mainViewModel.isWeightedGraph = false
+                            mainViewModel.isDirectedGraph = false
+                            val unloadAllEdges = tx.run(
+                                "MATCH (a:Vertex)-[edge:CONNECTED_TO]-(b:Vertex)" + "RETURN edge.type AS type, a.id AS idFrom, b.id AS idTo)",
+                            )
+                            unloadAllEdges.forEach { edge ->
+                                val IDFrom = edge["idFrom"].toString().toLong()
+                                val IDTo = edge["idTo"].toString().toLong()
+                                graphViewModel.createEdge(IDFrom, IDTo, null)
+                            }
+                        }
+
+                        else -> {
+                            throw IllegalArgumentException("Incorrect type of edge")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun uploadGraph() {
+        graphViewModel.clear()
+        uploadVertex {
+            uploadEdge {
+            }
+        }
+    }
+
 }
