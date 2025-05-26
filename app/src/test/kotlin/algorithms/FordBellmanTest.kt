fdaa42cd381a919f926886288010636613cf3700
silvitreatment <1junyawork@gmail.com>
2025-05-26 14:50:00 +0300

    init: FB tests

---
+++ app/src/test/kotlin/algorithms/FordBellmanTest.kt
@@ -0,0 +1,19 @@
+package test.algorithms
+
+import org.graphApp.model.graph.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class FordBellmanTest {
+    private lateinit var directGraph: DirectGraph<String,String>
+    private lateinit var undirectedGraph: UndirectedGraph<String, String>
+    private lateinit var weightedGraph: WeightedGraph<String, String>
+    private lateinit var directWeightedGraph: DirectWeightedGraph<String, String>
+
+
+
+
+}
